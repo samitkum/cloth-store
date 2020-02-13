ef0fb908335fc8235d4479011297e43f52e3ffde
Singh <samitkum@netapp.com>
2020-02-13 15:39:35 +0530

    added the cart redux and cart components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,8 +4,10 @@ import { ReactComponent as Logo } from "./crown.svg";
 import { Link } from "react-router-dom";
 import { auth } from "../../firebase/firebase.utils";
 import { connect } from "react-redux";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
   return (
     <div className="header">
       <Link className="logo-container" to="">
@@ -27,13 +29,16 @@ const Header = ({ currentUser }) => {
             Sign In
           </Link>
         )}
+        <CartIcon />
       </div>
+      {hidden ? null : <CartDropdown />}
     </div>
   );
 };
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden
 });
 
 export default connect(mapStateToProps)(Header);
--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -1,8 +1,9 @@
 import { combineReducers } from "redux";
-import currentUser from "./user/user.reducer";
 
-const RootReducer = combineReducers({
-  user: currentUser
+import userReducer from "./user/user.reducer";
+import cartReducer from "./cart/cart.reducer";
+
+export default combineReducers({
+  user: userReducer,
+  cart: cartReducer
 });
-
-export default RootReducer;
